fd098c63a9642a7f8b33939bd1ad9518cf0e61a5
wen-surface <l1048445160@live.com>
2020-12-11 18:02:50 +0800

    Fix leetcode solution 561, array partition i, format Solution.java, add test demo.

--- LeetCode/0561-array-partition-i/Solution.java
+++ LeetCode/0561-array-partition-i/Solution.java
@@ -1,14 +1,20 @@
 import java.util.Arrays;
 
-public class Solution {}
+public class Solution {
+	public static void main(String[] args) {
+		int[] arr = {1, 4, 3, 2};
+		Solution01 s1 = new Solution01();
+		System.out.println(s1.arrayPairSum(arr));  // 4
+	}
+}
 
 class Solution01 {
-    public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
-	int sum = 0;
-	for (int i = 0; i < nums.length; i += 2) {
-            sum += nums[i];
+	public int arrayPairSum(int[] nums) {
+		Arrays.sort(nums);
+		int sum = 0;
+		for (int i = 0; i < nums.length; i += 2) {
+			sum += nums[i];
+		}
+		return sum;
 	}
-	return sum;
-    }
 }
