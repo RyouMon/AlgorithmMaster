f8ac6e96f1c9900744bae230d019b704d65ca5e9
Wen <wenslife@outlook.com>
2021-11-11 12:21:35 +0800

    Fix Leetcode Solution0300 (Python)

--- LeetCode/0300-longest-increasing-subsequence/solutions.py
+++ LeetCode/0300-longest-increasing-subsequence/solutions.py
@@ -8,13 +8,15 @@ class Solution:
     
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = [1] * len(nums)
+        result = 1
         
         for i in range(1, len(nums)):
             for j in range(i):
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j] + 1)
+            result = max(result, dp[i])
         print(dp)
-        return dp[-1]
+        return result
 
 
 if __name__ == '__main__':
