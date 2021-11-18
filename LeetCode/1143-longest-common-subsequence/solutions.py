52087db499c6da4ced019b741074fec502b1ec39
Wen <wenslife@outlook.com>
2021-11-18 11:27:11 +0800

    Add Leetcode Solution 1143 (Python)

---
+++ LeetCode/1143-longest-common-subsequence/solutions.py
@@ -0,0 +1,23 @@
+class Solution:
+    """最长公共子序列 动态规划
+    时间复杂度O(mn) 空间复杂度O(mn)
+    """
+  
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1) + 1, len(text2) + 1
+        dp = [[0] * n for _ in range(m)]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        return dp[-1][-1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    result = solution.longestCommonSubsequence("abcba", "abcbcba")
+    assert result == 5
