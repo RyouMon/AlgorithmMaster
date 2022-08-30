fd0a1eb681d9ac37f3bcd5166ca3d6339e3624f3
Wen <wenslife@outlook.com>
2022-08-30 22:18:43 +0800

    新增 242. 有效的字母异位词 Java解法

---
+++ LeetCode/0242-valid-anagram/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * 242. 有效的字母异位词 字典解法
+ * 时间复杂度O(mn) 空间复杂度O(1)
+ */
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] record = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            record[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            record[t.charAt(i) - 'a']--;
+        }
+        
+        for (int count: record) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
